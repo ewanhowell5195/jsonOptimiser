2cf201b0f4ce9971843c140ea44886ba3ebc2385
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-08-08 21:17:36 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,22 +1,22 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 where winget > nul 2>&1
-if %errorlevel% neq 0 (
-    echo winget is not available. Please install Node.js manually.
-    pause
+if !errorlevel! neq 0 (
+  echo winget is not available. Please install Node.js manually.
+  pause
 ) else (
-    where node > nul 2>&1
-    if %errorlevel% neq 0 (
-        echo Node.js is not installed. Installing...
-        runas /user:Administrator "winget install -e --id OpenJS.Nodejs"
-        start "Reopening Batch Script" "%~dpnx0"
-        exit
-    ) else (
-        echo Node.js is already installed.
-    )
-    echo Installing Node.js modules...
-    npm install
-    echo Script completed.
-    pause
+  where node > nul 2>&1
+  if !errorlevel! neq 0 (
+    echo Node.js is not installed. Installing...
+    winget install -e --id OpenJS.NodeJS
+    start "Installing Node.js modules" "cmd /c %~dpnx0"
+    exit
+  ) else (
+    echo Node.js is already installed.
+  )
+  echo Installing Node.js modules...
+  npm install
+  echo Script completed.
+  pause
 )
