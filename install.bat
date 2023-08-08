a40346383b27613b854c4a01ee6473908429395a
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-08-08 21:43:39 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,13 +10,12 @@ if !errorlevel! neq 0 (
   if !errorlevel! neq 0 (
     echo Node.js is not installed. Installing...
     winget install -e --id OpenJS.NodeJS
-    start "Installing Node.js modules" "cmd /c %~dpnx0"
-    exit
+    for /f "tokens=* usebackq" %%p in (`powershell -Command "& {[System.Environment]::GetEnvironmentVariable('Path','Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path','User')}"`) do (set path=%%p)
   ) else (
     echo Node.js is already installed.
   )
   echo Installing Node.js modules...
   npm install
-  echo Script completed.
+  echo Install completed.
   pause
 )
