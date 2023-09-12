302e7cd0dbc877157074d7b5f92d8578edab8020
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-09-12 17:44:52 +0100

    1.20.2 fix

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function processPart(part, rootMode) {
   }
 }
 
-const mcmetaKeys = [ "credit", "animation", "villager", "texture", "pack", "language", "filter", "overlays" ]
+const mcmetaKeys = [ "credit", "animation", "villager", "texture", "pack", "language", "filter", "overlays", "gui" ]
 const animationKeys = [ "interpolate", "width", "height", "frametime", "frames" ]
 const jemKeys = [ "credit", "texture", "textureSize", "shadowSize", "models" ]
 const modelKeys = [ "model", "id", "part", "attach", "scale", "animations" ]
@@ -142,7 +142,7 @@ for await (const file of getFiles(dir)) {
     }
     if (data.pack) {
       for (const key in data.pack) {
-        if (!(key === "pack_format" || key === "supported_formats " || key === "description")) delete data.pack[key]
+        if (!(key === "pack_format" || key === "supported_formats" || key === "description")) delete data.pack[key]
       }
     } else if (data.animation) {
       for (const key in data.animation) {
